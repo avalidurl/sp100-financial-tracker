bad03614fb3607f508e0504e92b83572a896e27c
gokhan <0xgokhan@pm.me>
2025-10-03 19:35:07 +0300

    Remove Vercel config and API functions, using Cloudflare Pages only

--- api/cron/ping.js
+++
@@ -1,4 +0,0 @@
-module.exports = async function handler(req, res) {
-  return res.status(200).json({ ok: true, time: new Date().toISOString() });
-}
-
--- api/hello.js
+++
@@ -1,5 +0,0 @@
-module.exports = async function handler(req, res) {
-  res.setHeader('content-type', 'application/json');
-  return res.status(200).end(JSON.stringify({ message: 'Hello from Vercel API' }));
-}
-
